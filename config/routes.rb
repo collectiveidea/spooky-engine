b5a6201cfc0538ebadfe005ca702f0264d05e5d8
Daniel Morrison <daniel@collectiveidea.com>
2025-04-22 06:49:45 -0400

    Move root route to avoid pagination weirdnes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-Spooky::Engine.routes.draw do |x| 
+Spooky::Engine.routes.draw do |x|
   scope module: :spooky do
     resources :authors, only: [:index, :show]
     resources :pages, only: [:index, :show]
     resources :posts, only: [:show] # index is handled by the root
     resources :tags, only: [:index, :show]
-    get "feed.atom", to: "posts#index", defaults: {format: :atom}, as: :atom
     root to: "posts#index"
+    # Note: Having root below atom URL was making post pagination links go to feed instead.
+    get "feed.atom", to: "posts#index", defaults: {format: :atom}, as: :atom
   end
 end
